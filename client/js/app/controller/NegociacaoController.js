3582ec8782fb40ad6892c6f92f39fc4ea5d9f1e2
diegodamato <diegodamato6@gmail.com>
2017-07-23 00:04:25 -0300

    Finalizando a aula 4

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -5,24 +5,33 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
         
-        let data = new Date(
-            ...this._inputData.value
-                .split("-")
-                .map((item, indice) => item - indice % 2)                    
-            );
-        
-        let negociacao = new Negociacao(
-            data,
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        this._listaNegociacoes.negociacoes.length = 0;
+        console.log(this._listaNegociacoes.negociacoes);
+
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
-        
-        console.log(negociacao);
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
